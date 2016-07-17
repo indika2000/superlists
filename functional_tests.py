e595e9395cd875259b420b202abab49c45c4fa4c
indika <indika.attale@gmail.com>
2016-07-17 09:43:46 +0100

    First functional test using unittest and spec'd out comments

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,31 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
 
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #user hears about new list app which attempt to go to via the browser
+        self.browser.get('http://localhost:8000')
+
+        #They look at the title of the web page and make sure it is the list app
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        #They then are invited to enter their first item in a list
+
+        # ...
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
