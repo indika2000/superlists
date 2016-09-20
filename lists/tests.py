311604f410d60122006956d8a1dee785aed20ca6
indika <indika.attale@gmail.com>
2016-09-20 11:21:46 +0100

    finally got unit tests to work due to string for post request being mismatched

--- lists/tests.py
+++ lists/tests.py
@@ -21,13 +21,13 @@ class HomePageTest(TestCase):
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
-        request.POST['item_text'] = 'A new item list'
+        request.POST['item_text'] = 'A new list item'
 
         response = home_page(request)
 
-        self.assertIn('A new item list', response.content.decode())
+        self.assertIn('A new list item', response.content.decode())
         expected_html = render_to_string(
             'home.html',
             {'new_item_text': 'A new list item'}
         )
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(response.content.decode(), expected_html, "Generated text \n%s" % response.content.decode())
