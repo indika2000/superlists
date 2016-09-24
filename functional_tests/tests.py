344047dd4b20a674506e3dad48f01c567b6fc6aa
indika <indika.attale@gmail.com>
2016-09-24 08:02:09 +0100

    finally fixed the functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -43,13 +43,15 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
 
         #Now a new user, Francis, comes along to the site
 
@@ -61,7 +63,7 @@ class NewVisitorTest(LiveServerTestCase):
         #Francise visits the home page. There is no sign of Edith's
         #list
         self.browser.get(self.live_server_url)
-        page_text = self.browser.find_elements_by_tag_name('body').text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
